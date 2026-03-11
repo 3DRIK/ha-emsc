7460261b7e608566a7e382b041dbc05e4780cc89
3DRIK <96953423+3DRIK@users.noreply.github.com>
2026-03-11 09:07:19 +0100

    Create const.py

---
+++ cutom-components/seismicportal/const.py
@@ -0,0 +1,8 @@
+DOMAIN = "seismicportal"
+
+CONF_LAT = "latitude"
+CONF_LON = "longitude"
+CONF_RADIUS = "radius"
+CONF_MAG = "min_magnitude"
+
+WS_URL = "wss://www.seismicportal.eu/standing_order/websocket"
