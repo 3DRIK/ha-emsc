fb6ef522e0b12cf3ad07bb938ba439f8ea6fd9c9
3DRIK <96953423+3DRIK@users.noreply.github.com>
2026-03-11 09:20:46 +0100

    Update sensor.py

--- cutom-components/seismicportal/sensor.py
+++ cutom-components/seismicportal/sensor.py
@@ -1,33 +1,55 @@
 from homeassistant.components.sensor import SensorEntity
-from homeassistant.helpers.update_coordinator import CoordinatorEntity
-from .const import DOMAIN
+from homeassistant.core import callback
+
+from .const import DOMAIN, EVENT_EARTHQUAKE
+
 
 async def async_setup_entry(hass, entry, async_add_entities):
 
-    coordinator = hass.data[DOMAIN][entry.entry_id]
+    sensor = EarthquakeSensor(hass)
 
-    async_add_entities([SeismicSensor(coordinator)])
+    async_add_entities([sensor])
 
 
-class SeismicSensor(CoordinatorEntity, SensorEntity):
+class EarthquakeSensor(SensorEntity):
 
     _attr_name = "Earthquake"
     _attr_native_unit_of_measurement = "M"
 
-    def __init__(self, coordinator):
-        super().__init__(coordinator)
+    def __init__(self, hass):
+
+        self.hass = hass
+        self._state = None
+        self._attrs = {}
+
+    async def async_added_to_hass(self):
+
+        @callback
+        def handle_event(event):
+
+            data = event.data
+
+            self._state = data["magnitude"]
+
+            self._attrs = {
+                "distance_km": data["distance_km"],
+                "region": data["region"],
+                "lat": data["lat"],
+                "lon": data["lon"],
+                "time": data["time"]
+            }
+
+            self.async_write_ha_state()
+
+        self.hass.bus.async_listen(
+            EVENT_EARTHQUAKE,
+            handle_event
+        )
 
     @property
     def native_value(self):
-        if self.coordinator.data:
-            return self.coordinator.data["magnitude"]
+        return self._state
 
     @property
     def extra_state_attributes(self):
-        if not self.coordinator.data:
-            return {}
-
-        return {
-            "location": self.coordinator.data["location"],
-            "time": self.coordinator.data["time"]
-        }
+        return self._attrs
